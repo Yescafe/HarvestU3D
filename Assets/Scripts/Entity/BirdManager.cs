6a6a2bfcbb0b9b22de22d2485e4f9e5439ff05dd
HYDlink <508860736@qq.com>
2020-05-22 11:22:02 +0800

    Add empty entity scripts, singleton script

---
+++ Assets/Scripts/Entity/BirdManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdManager : DCLSingletonBase<BirdManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnBird() {
+        
+    }
+}

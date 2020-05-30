563da1c58959e8af6023694776a17de5f28bfc6b
Ivan Chien <qyc027@gmail.com>
2020-05-30 21:11:16 +0800

    添加一个简易的游戏信息输出板
    - 添加一个简易的游戏信息输出板
    - 追加伐木人胜利时的信息

---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGameManager : MonoBehaviour
+{
+    [NonSerialized] public bool isLumbererWon = false;
+    [NonSerialized] public bool isNatureWon = false;
+
+    public GameObject dispInfo;
+
+    public void UpdateStatus()
+    {
+        if (Trees.I.GetComponent<Transform>().childCount == 0)
+        {
+            isLumbererWon = true;
+            isNatureWon = !isLumbererWon;
+        }
+    }
+
+    public void Update()
+    {
+        UpdateStatus();
+        if (isLumbererWon)
+        {
+            dispInfo.GetComponent<Text>().text = "Lumberers Won!";
+        }
+    }
+}
